67d4a090254701e4242abc18a7834766a47c494b
NaskiAscii <160227029+NaskiAscii@users.noreply.github.com>
2025-01-09 19:33:55 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+enum RadioMessage {
+    message1 = 49434
+}
+/**
+ * Header
+ */
+/**
+ * Body
+ */
+function sendmessage (text: string) {
+    radio.setGroup(1)
+    radio.sendString("01" + SendMessageDestination + "XX" + text + "XX" + "10")
+}
+input.onButtonPressed(Button.A, function () {
+    sendmessage("12345678")
+})
+radio.onReceivedString(function (receivedString) {
+    messageDestination = "" + receivedString.charAt(2) + receivedString.charAt(3)
+    if (messageDestination == Id) {
+        RecievedMessageBody = "" + receivedString.charAt(6) + receivedString.charAt(7) + receivedString.charAt(8) + receivedString.charAt(9) + receivedString.charAt(10) + receivedString.charAt(11) + receivedString.charAt(12) + receivedString.charAt(13)
+        basic.showString(RecievedMessageBody)
+    } else {
+        basic.showString("No")
+    }
+})
+let RecievedMessageBody = ""
+let messageDestination = ""
+let SendMessageDestination = ""
+let Id = ""
+Id = "00"
+SendMessageDestination = "00"
